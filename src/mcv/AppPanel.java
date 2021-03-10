5fa0d49c79f44f0ac65a132ae975586dc0ad5bb4
CanHuynh <canhuynh1998@gmail.com>
2021-03-09 17:23:25 -0800

    Initial commit

---
+++ src/mcv/AppPanel.java
@@ -0,0 +1,4 @@
+package mcv;
+
+public class AppPanel {
+}
---
+++ src/mcv/Model.java
@@ -0,0 +1,10 @@
+package mcv;
+
+public class Model {
+
+    public String getFileName(){
+        return "";
+    }
+
+    public void setFileName(String fileName){}
+}
