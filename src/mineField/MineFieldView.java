acac6b2bdf63baa805984915a3a4bc4a499b7870
paulsoriiiano <53189056+paulsoriiiano@users.noreply.github.com>
2021-03-26 13:42:23 -0700

    Debugged and overrode propertyChanged() in MineFieldView.

--- src/mineField/MineFieldView.java
+++ src/mineField/MineFieldView.java
@@ -1,6 +1,5 @@
 package Minefield;
 
-import mvc.Model;
 import mvc.View;
 
 import javax.swing.*;
@@ -11,41 +10,55 @@ import java.beans.PropertyChangeEvent;
 /*
  *  Hoc Can 3/15/2021: created file
  *  Hoc Can 3/21/2021: Initialized constructor
+ *  Paul Soriano 3/22/2021: Overrode propertyChanged().
  */
 public class MineFieldView extends View {
-    private Model model;
+    private MineField model;
     private static int GRIDSIZE = 20;
     private JLabel[][] grids;
-    public MineFieldView(Model mod) {
+    private int newRow;
+    private int newCol;
+    final Border BLACKLINE = BorderFactory.createLineBorder(Color.BLACK);
+    final Border WHITELINE = BorderFactory.createLineBorder(Color.WHITE);
+    final Border GREENLINE = BorderFactory.createLineBorder(Color.GREEN);
+    public MineFieldView(MineField mod) {
         super(mod);
-        model = new MineField();
+        model = mod;
         grids = new JLabel[GRIDSIZE][GRIDSIZE];
         this.setLayout(new GridLayout(GRIDSIZE, GRIDSIZE));
-        Border blackLine = BorderFactory.createLineBorder(Color.BLACK);
-        Border whiteLine = BorderFactory.createLineBorder(Color.WHITE);
-        Border greenLine = BorderFactory.createLineBorder(Color.GREEN);
+
 
         //Initial
         for(int i = 0; i< GRIDSIZE;i++){
             for(int j = 0; j < GRIDSIZE; j++){
                 grids[i][j] = new JLabel("?");
-                grids[i][j].setBorder(blackLine);
+                grids[i][j].setBorder(BLACKLINE);
                 this.add(grids[i][j]);
             }
         }
-        // Set exit as green
-        grids[GRIDSIZE-1][GRIDSIZE-1].setBorder(greenLine);
+        // Set exit as green.
+        grids[GRIDSIZE-1][GRIDSIZE-1].setBorder(GREENLINE);
+
+        // Set starting location.
+        grids[0][0].setBorder(WHITELINE);
+        grids[0][0].setBackground(Color.BLUE);
+        grids[0][0].setText(String.valueOf(model.field[0][0].getMinedNeighbors()));
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        //TODO
-       if (evt.getPropertyName().equals("visited")){
-           for(int i = 0; i< GRIDSIZE;i++) {
-               for (int j = 0; j < GRIDSIZE; j++) {
-
-               }
-           }
-       }
+        if (evt.getPropertyName().equals("locationX")){
+            newRow = (Integer) evt.getNewValue();
+        }
+        if (evt.getPropertyName().equals("locationY")){
+            newCol = (Integer) evt.getNewValue();
+        }
+        if (evt.getPropertyName().equals("visited")){
+            grids[newCol][newRow].setBorder(WHITELINE);
+            grids[newCol][newRow].setBackground(Color.BLUE);
+            grids[newCol][newRow].setText(String.valueOf(model.field[newCol][newRow].getMinedNeighbors()));
+            // For debugging
+            System.out.printf("View Changed. X: %d, Y: %d%n", newRow, newCol);
+        }
     }
 }
